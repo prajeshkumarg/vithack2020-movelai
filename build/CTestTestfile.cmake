d25ab1e66614b40f432d00fe910670ba576524df
prajeshkumarg <prajeshofficial007@gmail.com>
2020-10-11 01:46:57 +0530

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,18 @@
+# CMake generated Testfile for 
+# Source directory: /home/prajesh/Desktop/catkin_ws/src
+# Build directory: /home/prajesh/Desktop/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("pocketsphinx")
+subdirs("turtlebot3")
+subdirs("turtlebot3_navigation")
+subdirs("turtlebot3_simulations/turtlebot3_simulations")
+subdirs("turtlebot3_bringup")
+subdirs("turtlebot3_example")
+subdirs("turtlebot3_simulations/turtlebot3_fake")
+subdirs("turtlebot3_simulations/turtlebot3_gazebo")
+subdirs("turtlebot3_slam")
+subdirs("turtlebot3_teleop")
+subdirs("turtlebot3_description")
